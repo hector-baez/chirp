52756fd3299576ddd1918e7013c7ac11f2c7f8a1
hector-baez <hbaez@oxy.edu>
2025-11-30 23:23:12 -0600

    Implement EmailVerificationLogic

--- user/src/main/kotlin/com/hbaez/chirp/infra/database/repositories/EmailVerificationTokenRepository.kt
+++ user/src/main/kotlin/com/hbaez/chirp/infra/database/repositories/EmailVerificationTokenRepository.kt
@@ -2,9 +2,10 @@ package com.hbaez.chirp.infra.database.repositories
 
 import com.hbaez.chirp.infra.database.entities.EmailVerificationTokenEntity
 import com.hbaez.chirp.infra.database.entities.UserEntity
+import org.springframework.data.jpa.repository.JpaRepository
 import java.time.Instant
 
-interface EmailVerificationTokenRepository {
+interface EmailVerificationTokenRepository : JpaRepository<EmailVerificationTokenEntity, Long> {
     fun findByToken(token: String): EmailVerificationTokenEntity?
     fun deleteByExpiresAtLessThan(now: Instant)
     fun findByUserAndUsedAtIsNull(user: UserEntity): List<EmailVerificationTokenEntity>
---
+++ user/src/main/kotlin/com/hbaez/chirp/service/auth/EmailVerificationService.kt
@@ -0,0 +1,76 @@
+package com.hbaez.chirp.service.auth
+
+import com.hbaez.chirp.domain.exception.InvalidTokenException
+import com.hbaez.chirp.domain.exception.UserNotFoundException
+import com.hbaez.chirp.domain.model.EmailVerificationToken
+import com.hbaez.chirp.infra.database.entities.EmailVerificationTokenEntity
+import com.hbaez.chirp.infra.database.mappers.toEmailVerificationToken
+import com.hbaez.chirp.infra.database.mappers.toUser
+import com.hbaez.chirp.infra.database.repositories.EmailVerificationTokenRepository
+import com.hbaez.chirp.infra.database.repositories.UserRepository
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+import java.util.concurrent.TimeUnit
+
+
+@Service
+class EmailVerificationService(
+    private val emailVerificationTokenRepository: EmailVerificationTokenRepository,
+    private val userRepository: UserRepository,
+    @param:Value("\${chirp.email.verification.expiry-hours}") private val expiryHours: Long
+) {
+
+    @Transactional
+    fun createVerificationToken(email: String): EmailVerificationToken {
+        val userEntity = userRepository.findByEmail(email)
+            ?: throw UserNotFoundException()
+        val existingTokens = emailVerificationTokenRepository.findByUserAndUsedAtIsNull(
+            user = userEntity
+        )
+        val now = Instant.now()
+        val usedTokens = existingTokens.map {
+            it.apply {
+                this.usedAt = now
+            }
+        }
+        emailVerificationTokenRepository.saveAll(usedTokens)
+        val token = EmailVerificationTokenEntity(
+            expiresAt = now.plus(expiryHours, ChronoUnit.HOURS),
+            user = userEntity
+        )
+        return emailVerificationTokenRepository.save(token).toEmailVerificationToken()
+    }
+
+    @Transactional
+    fun verifyEmail(token: String) {
+        val verificationToken = emailVerificationTokenRepository.findByToken(token)
+            ?: throw InvalidTokenException("Email verification token is invalid.")
+        if(verificationToken.isUsed) {
+            throw InvalidTokenException("Email verification token is already used.")
+        }
+        if(verificationToken.isExpired) {
+            throw InvalidTokenException("Email verification token has already expired.")
+        }
+        emailVerificationTokenRepository.save(
+            verificationToken.apply {
+                this.usedAt = Instant.now()
+            }
+        )
+        userRepository.save(
+            verificationToken.user.apply {
+                this.hasVerifiedEmail = true
+            }
+        ).toUser()
+    }
+
+    @Scheduled(cron = "0 0 3 * * *")
+    fun cleanupExpiredTokens() {
+        emailVerificationTokenRepository.deleteByExpiresAtLessThan(
+            now = Instant.now()
+        )
+    }
+}
